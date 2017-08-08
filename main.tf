65ef9366bbe955a7ebb3c8a2de9c640f0b239e15
Sergey Vasilyev <s2504s@gmail.com>
2017-08-08 10:11:26 +0300

    Pinned to v0.1.0. Changed bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "tf_label" {
-  source    = "git::https://github.com/cloudposse/tf_label.git?ref=master"
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   name      = "${var.name}"
