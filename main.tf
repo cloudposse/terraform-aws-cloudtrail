d3ed33ca10ad18894aa3fc2f2fc6aff668b6a9ce
Valeriy Onishchenko <drama17@gmail.com>
2017-08-18 19:31:44 +0300

    fix wrong link for the source

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "tf_label" {
-  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   name      = "${var.name}"
