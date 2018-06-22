4feaeac2faf08296bf1e14c9e4cf3370ed9120f4
Jamie Nelson <jamie@theinstillery.com>
2018-06-22 19:33:54 +0100

    Updated fields, and changed to terraform-terraform-label

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "cloudtrail_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.2"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
@@ -18,6 +18,6 @@ resource "aws_cloudtrail" "default" {
   cloud_watch_logs_role_arn     = "${var.cloud_watch_logs_role_arn}"
   cloud_watch_logs_group_arn    = "${var.cloud_watch_logs_group_arn}"
   tags                          = "${module.cloudtrail_label.tags}"
-  event_selector                = "${var.event_selector}"
+  event_selector                = ["${var.event_selector}"]
   kms_key_id                    = "${var.kms_key_id}"
 }
