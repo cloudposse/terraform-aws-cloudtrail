45fbc6b294bc3086d553ae68f2f02b4ff35292bc
vadim-hleif <vadzim.hleif@gmail.com>
2018-06-26 16:36:23 +0300

    refactor(readme): migrate to yaml format

---
+++ docs/targets.md
@@ -0,0 +1,10 @@
+## Makefile Targets
+```
+Available targets:
+
+  help                                This help screen
+  help/all                            Display help for all targets
+  lint:
+                              Lint terraform code
+
+```
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "cloudtrail_id" {
   value = "${aws_cloudtrail.default.id}"
+  description = "The name of the trail"
 }
 
 output "cloudtrail_home_region" {
   value = "${aws_cloudtrail.default.home_region}"
+  description = "The region in which the trail was created"
 }
 
 output "cloudtrail_arn" {
   value = "${aws_cloudtrail.default.arn}"
+  description = "The Amazon Resource Name of the trail"
 }
