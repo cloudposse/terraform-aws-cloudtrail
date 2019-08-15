345339495898fe4d6dc304e9e3bcfcbb4a5162f8
Igor Rodionov <goruha@gmail.com>
2019-08-15 17:33:27 +0600

    Rename KMS key (#27)

--- main.tf
+++ main.tf
@@ -21,6 +21,6 @@ resource "aws_cloudtrail" "default" {
   cloud_watch_logs_group_arn    = "${var.cloud_watch_logs_group_arn}"
   tags                          = "${module.cloudtrail_label.tags}"
   event_selector                = "${var.event_selector}"
-  kms_key_id                    = "${var.kms_key_id}"
+  kms_key_id                    = "${var.kms_key_arn}"
   is_organization_trail         = "${var.is_organization_trail}"
 }
